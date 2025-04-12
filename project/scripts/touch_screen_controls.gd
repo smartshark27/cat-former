1bc25dba6ce1af6f530e1672a190378e923c89a6
Thomas Smart <thomassmart@Thomass-MacBook-Pro.local>
2025-04-12 18:19:11 +1000

    Fix touch controls

--- project/scripts/touch_screen_controls.gd
+++ project/scripts/touch_screen_controls.gd
@@ -1,31 +1,25 @@
 extends Node2D
 
 
-func _on_left_button_button_down() -> void:
-	print("left down")
+func _on_left_button_pressed() -> void:
 	Input.action_press("left")
 
 
-func _on_left_button_button_up() -> void:
-	print("left up")
+func _on_left_button_released() -> void:
 	Input.action_release("left")
 
 
-func _on_right_button_button_down() -> void:
-	print("right down")
+func _on_right_button_pressed() -> void:
 	Input.action_press("right")
 
 
-func _on_right_button_button_up() -> void:
-	print("right up")
+func _on_right_button_released() -> void:
 	Input.action_release("right")
 
 
-func _on_jump_button_button_down() -> void:
-	print("jump down")
+func _on_jump_button_pressed() -> void:
 	Input.action_press("jump")
 
 
-func _on_jump_button_button_up() -> void:
-	print("jump up")
+func _on_jump_button_released() -> void:
 	Input.action_release("jump")
